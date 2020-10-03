8966ba4878f4fb5e6fc496208947f4de89274496
ashleyjr <ashley181291@gmail.com>
2020-10-03 10:47:41 +0100

    tidy up image gen

--- makefile
+++ makefile
@@ -2,17 +2,19 @@ SCALE=10000
 LOG2N=2
 
 all:
-	rm -rf common/Ws.h
+	rm -rf common/Ws.h	
+	python tools/imageGen/imageGen.py --line1 fft --line2 8051 --var disp_fft > disp/disp.h	
+	python tools/imageGen/imageGen.py --line1 TEST --size 20  --var disp_test >> disp/disp.h
 	python common/lib/python/Ws.py > common/Ws.h ${LOG2N} ${SCALE}	
 	sdcc -c common/fft8051.c -I common/ -DLOG2N=${LOG2N} -DSCALE=${SCALE}
-	sdcc  src/main.c fft8051.rel -I inc/ -I common/ -DLOG2N=${LOG2N} -DSCALE=${SCALE} --iram-size 256 --xram-size 256 	
+	sdcc  src/main.c fft8051.rel -I inc/ -I common/ -I disp/ -DLOG2N=${LOG2N} -DSCALE=${SCALE} --iram-size 256 --xram-size 256 	
 
 compare:
 	rm -rf common/Ws.h
 	python common/lib/python/Ws.py > common/Ws.h ${LOG2N} ${SCALE}
 	gcc -o model.o -pedantic model/src/main.c common/fft8051.c -I common/ -DLOG2N=${LOG2N} -DSCALE=${SCALE}
 	sdcc -c common/fft8051.c -I common/ -DLOG2N=${LOG2N} -DSCALE=${SCALE}
-	sdcc  src/main.c fft8051.rel -I inc/ -I common/ -DCOMPARE -DLOG2N=${LOG2N} -DSCALE=${SCALE} --iram-size 256 --xram-size 256 	
+	sdcc  src/main.c fft8051.rel -I inc/ -I common/ -I disp/ -DCOMPARE -DLOG2N=${LOG2N} -DSCALE=${SCALE} --iram-size 256 --xram-size 256 	
 
 clean:
 	rm -f common/Ws.h
--- tools/imageGen/imageGen.py
+++ tools/imageGen/imageGen.py
@@ -2,38 +2,45 @@ import sys
 import os
 import numpy
 from PIL import Image, ImageDraw, ImageFont
+import argparse
 
+FONTFILE = 'tools/imageGen/Dance.ttf'
+MAX      = 255
+THRESH   = MAX /2
+X        = 32
+Y        = 16
 
-def main(argv):
+def main(line1, line2, fontsize, var):
     # name of the file to save
-    filename = "img02.png"
-    fnt = ImageFont.truetype('Dance.ttf', 14)
+    fnt = ImageFont.truetype(FONTFILE, fontsize)
     # create new image
-    image = Image.new(mode = "RGB", size = (32,16), color = "black")
+    image = Image.new(mode = "RGB", size = (X,Y), color = "black")
     draw = ImageDraw.Draw(image)
-    draw.text((0,0), "TEST", font=fnt, fill=(255,255,255))
-    # save the file
-    image.save(filename)
-
-
-    m = numpy.asarray(Image.open(filename).convert('L'))
-
-    print m
-
-    for i in range(31,-1,-1):
+    draw.text((0,0),    line1, font=fnt, fill=(MAX,MAX,MAX))
+    draw.text((0,Y/2),  line2, font=fnt, fill=(MAX,MAX,MAX))
+    # Turn to numpy
+    m = numpy.asarray(image.convert('L'))
+    # Print the code
+    print "static const U16 "+var+"["+str(X)+"] = {"
+    for i in range(X-1,-1,-1):
         s = "0b"
-        for j in range(15,-1,-1):
-            if(m[j,i] > 128):
+        for j in range(Y-1,-1,-1):
+            if(m[j,i] > THRESH):
                 s += '0'
             else:
                 s += '1'
         if(i != 0):
             s += ','
         print s
-
-
+    print "};"
     return
 
 if "__main__" == __name__:
-    main(sys.argv)
+    parser = argparse.ArgumentParser(description='fft8051 display text gen.')
+    parser.add_argument('--line1',  default="",     type=str,   help='Top line')
+    parser.add_argument('--line2',  default="",     type=str,   help='Bottom line')
+    parser.add_argument('--size',   default="10",   type=int,   help='Size of font')
+    parser.add_argument('--var',    default="disp", type=str,   help='Variable name')
+    args = parser.parse_args()
+    main(args.line1, args.line2, args.size, args.var)
 
